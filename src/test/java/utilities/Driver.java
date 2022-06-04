70a71ac08606d1afffe0431e3eb663f52ac9aecf
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-06-04 23:43:18 +0300

    Initial commit

---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,85 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    /*
+    POM'de Driver icin  TestBase class'ina extends etmek yerine
+    Driver class'indan static method'lar kullanarak
+    driver olusturup, ilgili ayarlarin yapilmasi
+    ve en sonda driver'in kapatilmasi tercih edilir
+
+    POM'de Driver class'indaki getDriver()'in olusturularak kullanilmasini engellemek icin
+    Singleton pattern kullanimi benimsenmistir
+
+    Singleton Pattern: tekli kullanim, bir classin farkli class'laerdan
+    obje olusturularak kullanimini engellemek icin kullanilir
+
+    Bunu saglamak icin yapmamiz gereken sey oldukca basit
+    obje olusturmak icin kullanilan constructor'i private yaptiginizda
+    baska class'lardan Driver classindan obje olusturulmasi mumkun OLAMAZ
+
+     */
+
+    private Driver(){
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver() {
+
+        if (driver == null){
+
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver= new SafariDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                case "egde":
+                    WebDriverManager.edgedriver().setup();
+                    driver=new EdgeDriver();
+                    break;
+                case "opera":
+                    WebDriverManager.operadriver().setup();
+                    driver=new OperaDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+            }
+
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+
+        if (driver!=null){
+            driver.close();
+            driver=null;
+        }
+
+    }
+
+}
